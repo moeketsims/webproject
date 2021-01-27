624a7ede998adf70154adaa8a2e3b10364afb181
moeketsims <bricoleurmosia@gmail.com>
2021-01-27 22:59:36 +0200

    LA

--- ui.R
+++ ui.R
@@ -1,54 +1,42 @@
 library(shiny)
 library(shinyWidgets)
 
-ui <- fluidPage(id="body",
-
+ui <- bootstrapPage(id="body",
+                    
   includeCSS("www/custom.css"),
-  
-  fluidRow(style = "background-image: url(https://images.unsplash.com/photo-1536528679846-3c5a38e3026b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YmFja2dyb3VuZCUyMGNvdmVyfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80);
+  img = "https://www.wallpapertip.com/wmimgs/83-838296_web-designer-professional-website-background-images.jpg",
+  fluidRow(style = "background-image: url(https://uploads.sitepoint.com/wp-content/uploads/2016/10/1477467367r-shiny.jpg);
             backgraound-color:grey;
             background-repeat: no-repeat;
             background-position: center center;
             background-size:cover;
             
            ",
-    
+           theme="https://stackpath.bootstrapcdn.com/bootswatch/3.4.1/flatly/bootstrap.min.css",
     div(class="box",
         div(class="header",
             tags$img(src="logo.PNG")),
         
+        # tags$nav(
+        #   tags$link(rel="stylesheet", type="text/css", href="bootstrap.min.css")
+        # ),
+        # 
         div(class="nav",
               mainPanel(
                 tabsetPanel(type="tabs",
-                            tabPanel("Home",
-                                     
-                                     
-                                     
-                                     ),
+                            tabPanel("Home"),
                             tabPanel("Moodle Analytics"),
                             tabPanel("Student Geo-maping"),
-                            tabPanel("Predictive Analytic")
+                            tabPanel("Predictive Analytics"),
+                            tabPanel("Student Engagement")
                 ),
                 class="flex-center"
               )
             ),
+        
         div(class="main",
             tags$br(),
-            h1("Let's shiny with R Shiny."),
-            tags$br(),
-            tags$br(),
-            tags$br(),
-            tags$br(),
-            tags$br(),
-            tags$br(),
-            tags$br(),
-            tags$br(),
-      
-            p("I created this Shiny dashboard for my work and I am sharing it for two reasons, (i) showcase what is possible with R progarmming,(ii) to help those starting out with buidling dashbooards in R shiny."),
-            tags$a(href="https://github.com/moeketsims/webproject", "Click Here to Check my Code on GitHub"),
-            tags$br(),
-            tags$a(href="mailto:bricoleurmosia@gmail.com", "Drop me an Email Here if you want 
-                   to Collaborate in Developing dashboard with R shiny, Python or Machine Learning in General")
+            h1("Let's shiny with R Shiny.")
         ),
         div(class="footer",
             p("Moeketsi Mosia - Copyright 2021©")
